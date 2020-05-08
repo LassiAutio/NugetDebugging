837c27806c1be10d13b1c0557b38e5e0c6750f2c
Lassi Autio <lassi.autio@gmail.com>
2020-05-08 21:56:16 +0300

    Initial MyNuget

---
+++ MyNuget/MyHome.cs
@@ -0,0 +1,25 @@
+﻿namespace MyNuget
+{
+    public class MyHome
+    {
+        public int YearOfConstruction { get; set; }
+
+        public string GetCondition()
+        {
+            if (YearOfConstruction == 2020)
+            {
+                return "It is a brand new home!";
+            }
+            else if (YearOfConstruction > 2010)
+            {
+                return "Quite new home";
+            }
+            else if (YearOfConstruction < 2000)
+            {
+                return "It is from past century";
+            }
+
+            return "ERROR";
+        }
+    }
+}
