76775fd64d32bca05e3b983539a8bc31ba9c968b
Lassi Autio <lassi.autio@gmail.com>
2020-05-08 21:45:32 +0300

    Initial MyNugetConsumer console app

---
+++ MyNugetConsumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyNugetConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This console app will use custom nuget to show how to debug it.");
+        }
+    }
+}
